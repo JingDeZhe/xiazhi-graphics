ae5f981ccedfb0853484562bcc6ccf91381bb50e
lotusloli <nymphaeal@qq.com>
2024-06-04 16:05:26 +0800

    1. three 03

--- pages/three-03/main.js
+++ pages/three-03/main.js
@@ -1,4 +1,5 @@
 import './main.css'
+import { emitter } from './ui'
 const app = document.querySelector('#app')
 
 import * as THREE from 'three'
@@ -8,28 +9,39 @@ function init(cw, ch) {
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(cw, ch)
   app.appendChild(renderer.domElement)
+  /**@type {THREE.Mesh} */
+  let imageMesh
 
   const scene = new THREE.Scene()
   const camera = new THREE.OrthographicCamera(-cw / 2, cw / 2, ch / 2, -ch / 2, 1, 1000)
   camera.position.z = 10
 
-  const loader = new THREE.TextureLoader()
-  loader.load('/imgs/3.jpg', (texture) => {
-    texture.magFilter = THREE.LinearFilter
-    texture.minFilter = THREE.LinearFilter
+  loadImage('/imgs/3.jpg')
+  renderer.setAnimationLoop(animate)
+  emitter.on('change-image', loadImage)
 
-    const geometry = new THREE.PlaneGeometry(cw, ch)
-    const materal = new THREE.MeshBasicMaterial({
-      map: texture,
+  function loadImage(url) {
+    const loader = new THREE.TextureLoader()
+    loader.load(url, (texture) => {
+      texture.magFilter = THREE.LinearFilter
+      texture.minFilter = THREE.LinearFilter
+
+      const geometry = new THREE.PlaneGeometry(cw, ch)
+      const material = new THREE.MeshBasicMaterial({
+        map: texture,
+      })
+      if (imageMesh) {
+        imageMesh.material = material
+      } else {
+        imageMesh = new THREE.Mesh(geometry, material)
+        scene.add(imageMesh)
+      }
     })
-    const imageMesh = new THREE.Mesh(geometry, materal)
-    scene.add(imageMesh)
-  })
+  }
 
   function animate() {
     renderer.render(scene, camera)
   }
-  renderer.setAnimationLoop(animate)
 
   window.addEventListener('resize', resize)
   function resize() {
--- pages/three-03/ui.js
+++ pages/three-03/ui.js
@@ -1,1 +1,33 @@
 import GUI from 'lil-gui'
+import mitt from 'mitt'
+const gui = new GUI()
+export const emitter = mitt()
+
+const fileInput = document.createElement('input')
+fileInput.type = 'file'
+fileInput.accept = 'image/*'
+fileInput.hidden = 'true'
+document.body.appendChild(fileInput)
+fileInput.addEventListener('change', (e) => {
+  const file = e.target.files[0]
+  if (!file) return
+  readFileAsUrl(file, (url) => {
+    emitter.emit('change-image', url)
+  })
+})
+const config = {
+  'Change Image': () => {
+    fileInput.click()
+  },
+}
+
+gui.add(config, 'Change Image')
+
+function readFileAsUrl(file, cb) {
+  if (!file || !cb) return
+  const reader = new FileReader()
+  reader.onloadend = () => {
+    cb(reader.result)
+  }
+  reader.readAsDataURL(file)
+}
